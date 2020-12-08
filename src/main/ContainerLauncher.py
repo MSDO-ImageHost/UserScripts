10bb3aac2d335f42cecb4e209fe02c074fd6fad9
Jakob Schøler <35096865+deadshotcalamity@users.noreply.github.com>
2020-12-08 01:31:11 +0100

    ContainerLauncher
    Adjusted names of tests and unified them in single class with single setUp
    
    Correctly applied error checking for missing file

--- src/main/ContainerLauncher.py
+++ src/main/ContainerLauncher.py
@@ -12,28 +12,41 @@ class Container:
 
     def container_starter(self, language, file):
         method_name = str(language) + '_container'
-        method = getattr(self, method_name, lambda: "Invalid Language")
+        invalid_method_name = 'Invalid Language'
+        method = getattr(self, method_name, invalid_method_name)
+        if method == invalid_method_name:
+            return invalid_method_name
         return method(file)
 
     def python_container(self, file):
-        output = self.client.containers.run("python", "python mnt/src/test.py", volumes=self.volumes, remove=True)
+        try:
+            output = self.client.containers.run("python", "python mnt/src/" + file, volumes=self.volumes, remove=True)
+        except:
+            output = "File error"
         return output
 
     def java_container(self, file):
         container = self.client.containers.create(image="openjdk", volumes=self.volumes, working_dir='/mnt/src')
         container.start()
-        result = container.exec_run('javac ' + file)    #TODO compiled class file in test_scripts?
+        result = container.exec_run('javac ' + file)  # TODO compiled class file in test_scripts?
         name = split_name_and_type(file)
         output = container.exec_run('java ' + name)
         container.stop()
         container.remove()
+        if output.exit_code != 0:
+            return "File error"
         return output
 
     def haskell_container(self, file):
-        output = self.client.containers.run("haskell", "runhaskell mnt/src/test.hs", volumes=self.volumes, remove=True)
+        try:
+            output = self.client.containers.run("haskell", "runhaskell mnt/src/" + file, volumes=self.volumes, remove=True)
+        except:
+            output = "File error"
         return output
 
     def javascript_container(self, file):
-        output = self.client.containers.run("node", "node mnt/src/test.js", volumes=self.volumes, remove=True)
+        try:
+            output = self.client.containers.run("node", "node mnt/src/" + file, volumes=self.volumes, remove=True)
+        except:
+            output = "File error"
         return output
-
--- src/test/test_ContainerLauncher.py
+++ src/test/test_ContainerLauncher.py
@@ -23,66 +23,80 @@ class TestStartContainer(TestCase):
         root_dir = os.path.dirname(os.path.abspath("README.md"))
         self.volume_path = root_dir + "/src/test/test_scripts"
 
-    def test_start_python_container(self):
+    def test_container_starter_python(self):
         c = Container(self.volume_path)
         actual = c.container_starter("python", "test.py")
         expected = b'Hello, World!\n'
         self.assertEqual(expected, actual)
 
-    def test_start_java_container(self):
+    def test_container_starter_java(self):
         c = Container(self.volume_path)
         actual = c.container_starter("java", "test.java")
         expected = b'Hello, World!\n'
         self.assertEqual(expected, actual.output)
 
-    def test_start_haskell_container(self):
+    def test_container_starter_haskell(self):
         c = Container(self.volume_path)
         actual = c.container_starter("haskell", "test.hs")
         expected = b'Hello, World!\n'
         self.assertEqual(expected, actual)
 
-    def test_start_javascript_container(self):
+    def test_container_starter_javascript(self):
         c = Container(self.volume_path)
         actual = c.container_starter("javascript", "test.js")
         expected = b'Hello, World!\n'
         self.assertEqual(expected, actual)
 
+    def test_container_starter_invalid_Language(self):
+        c = Container(self.volume_path)
+        actual = c.container_starter("something that isn't a language", "test.js")
+        expected = "Invalid Language"
+        self.assertEqual(expected, actual)
 
-class TestPythonContainer(TestCase):
     def test_python_container(self):
-        root_dir = os.path.dirname(os.path.abspath("README.md"))
-        volume_path = root_dir + "/src/test/test_scripts"
-        c = Container(volume_path)
+        c = Container(self.volume_path)
         actual = c.python_container("test.py")
         expected = b'Hello, World!\n'
         self.assertEqual(expected, actual)
 
+    def test_python_container_invalid_file(self):
+        c = Container(self.volume_path)
+        actual = c.python_container("notAfile.notAtype")
+        expected = "File error"
+        self.assertEqual(expected, actual)
 
-class TestJavaContainer(TestCase):
     def test_java_container(self):
-        root_dir = os.path.dirname(os.path.abspath("README.md"))
-        volume_path = root_dir + "/src/test/test_scripts"
-        c = Container(volume_path)
+        c = Container(self.volume_path)
         actual = c.java_container("test.java")
         expected = b'Hello, World!\n'
         self.assertEqual(expected, actual.output)
 
+    def test_java_container_invalid_file(self):
+        c = Container(self.volume_path)
+        actual = c.java_container("notAfile.notAtype")
+        expected = "File error"
+        self.assertEqual(expected, actual)
 
-class TestHaskellContainer(TestCase):
     def test_haskell_container(self):
-        root_dir = os.path.dirname(os.path.abspath("README.md"))
-        volume_path = root_dir + "/src/test/test_scripts"
-        c = Container(volume_path)
+        c = Container(self.volume_path)
         actual = c.haskell_container("test.hs")
         expected = b'Hello, World!\n'
         self.assertEqual(expected, actual)
 
+    def test_haskell_container_invalid_file(self):
+        c = Container(self.volume_path)
+        actual = c.haskell_container("notAfile.notAtype")
+        expected = "File error"
+        self.assertEqual(expected, actual)
 
-class TestJavascriptContainer(TestCase):
     def test_javascript_container(self):
-        root_dir = os.path.dirname(os.path.abspath("README.md"))
-        volume_path = root_dir + "/src/test/test_scripts"
-        c = Container(volume_path)
+        c = Container(self.volume_path)
         actual = c.javascript_container("test.js")
         expected = b'Hello, World!\n'
+        self.assertEqual(expected, actual)
+
+    def test_javascript_container_invalid_file(self):
+        c = Container(self.volume_path)
+        actual = c.javascript_container("notAfile.notAtype")
+        expected = "File error"
         self.assertEqual(expected, actual)
